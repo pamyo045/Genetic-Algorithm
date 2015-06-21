0d171b3114a534450ea84d5b5a62dbe0d9a699e4
pamyo045 <amyot.pat@gmail.com>
2015-06-20 20:04:13 -0400

    .

--- GA_CO2Isotherm.m
+++ GA_CO2Isotherm.m
@@ -1,4 +1,4 @@
-function fun = GA_CO2Isotherm_ver1
+function fun = GA_CO2Isotherm
     % XLSname: Binary - N2-CO2 with HISIV3000 silicalite combo graph.xlsx
     % XLSsheet: Kp exp - curve fits
     % XLSrange: A4:F22
--- GA_CO2Isotherm_PlotOptPars.m
+++ GA_CO2Isotherm_PlotOptPars.m
@@ -1,4 +1,4 @@
-function fun = GA_CO2Isotherm_ver2
+function fun = GA_CO2Isotherm_PlotOptPars
     % XLSname: Binary - N2-CO2 with HISIV3000 silicalite combo graph.xlsx
     % XLSsheet: Kp exp - curve fits
     % XLSrange: A4:F22
